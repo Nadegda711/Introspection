88f0d2cfd409e7b84b4c4ce6bbad474a1cb96d60
Nadezda711 <semiliya711@mail.ru>
2024-11-25 17:10:25 +0500

    Add files via upload

--- module11_introspection.py
+++ module11_introspection.py
@@ -1,28 +1,18 @@
-from pprint import pprint
-
-
 def introspection_info(obj):
-    # Определяем тип объекта
     obj_type = type(obj).__name__
 
-    # Получаем все атрибуты
     all_attributes = dir(obj)
 
-    # Фильтруем атрибуты и методы
-    attributes = [attr for attr in all_attributes if not attr.startswith("__")]
     attributes = [attr for attr in all_attributes if callable(getattr(obj, attr)) and not attr.startswith("__")]
     methods = [method for method in all_attributes if callable(getattr(obj, method)) and method.startswith("__")]
-
-    # Получаем имя модуля
     module_name = __name__
 
-    # Формируем и возвращаем словарь с информацией
     return {
         'type': obj_type,
         'attributes': attributes,
         'methods': methods,
         'module': module_name
     }
-number_info = introspection_info(42)
 
+number_info = introspection_info(42)
 print(number_info)
